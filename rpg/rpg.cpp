842a7c2678eca6ee9129006aa8a27a40aacfc06f
Luca Seggiani <seggianiluca2004@gmail.com>
2023-10-18 18:34:00 +0200

    Aggiunta console e rivista interfaccia

--- rpg/rpg.cpp
+++ rpg/rpg.cpp
@@ -1,6 +1,6 @@
 #include <iostream> //input/output
 #include <cstdlib> //numeri casuali
-#include <ctime>
+#include <string.h> //stringhe/testo
 using namespace std;
 
 //tipi fondamentali
@@ -15,10 +15,28 @@ vettore2d boundLivelloMinimi = {5,5};
 vettore2d boundLivelloMassimi = {55, 15};
 
 //elementi menu
-struct elementoMenu { char ident; char* tag; void (*funzione)(int, Casella*, int&); }; 	//le funzioni accettano un indice i equivalente alla chiamata effettuata dal giocatore, questo
+int contatoreStringhe;
+struct elementoMenu { char ident; string tag; void (*funzione)(int, Casella*, int&); }; 	//le funzioni accettano un indice i equivalente alla chiamata effettuata dal giocatore, questo
 																						//è fatto per poter permettere comportamenti diversi alla solita funzione (e.g. spostaGiocatore)
 																						//inoltre accettano un riferimento alla stanza attuale e la dimensione orizzontale della suddetta
 const int elementiMenuMappa = 4; //fornisco una lunghezza fissa per il compilatore
+const int scrollbackConsole = 16; //imposta uno scrollback di 16
+string lineeConsole[scrollbackConsole];
+
+//utility shell
+void clear() {	//svuota lo schermo, multipiattaforma
+	#ifdef __CYGWIN32__
+		system("cls");
+	#else
+		system("clear");
+	#endif
+}
+void separator(int dim) {
+	for(int i = 0; i < dim * 2 + 32; i++) {
+		cout << "-";
+	}
+	cout << endl;
+}
 
 //utility livello
 int vettore2d_indice(vettore2d& vettore, int& dimX) { //converte un vettore bidimensionale in un indice array
@@ -54,8 +72,12 @@ void inizializza_stanza(Casella stanza[], int& dimX, int& dimY) {
 			bool muroDestro = (x == dimX - 1);
 			bool muroSuperiore = (y == 0);
 			bool muroInferiore = (y == dimY - 1);
+
+			bool portaX = (x == dimX/2);
+			bool portaY = (y == dimY/2);
+
 			//assegna enumeratore
-			if(muroSinistro|muroDestro|muroSuperiore|muroInferiore) {
+			if((muroSinistro|muroDestro|muroSuperiore|muroInferiore)&!(portaX|portaY)) {
 				casella -> tipoCasella = muro;
 			} else {
 				casella -> tipoCasella = casellaVuota;
@@ -97,6 +119,21 @@ void inizializza_menuMappa(elementoMenu menuMappa[elementiMenuMappa]) {
 	menuMappa[3].funzione = spostaGiocatore;
 }
 
+void inizializza_console() {
+	for(int i = 0; i < scrollbackConsole; i++) {
+		lineeConsole[i].resize(64); //evita segfault per stringhe troppo grandi
+	}
+}
+
+//interazione console
+void concatenaConsole(string nuovaStringa) {
+	for(int i = scrollbackConsole - 1; i >= 0; i--) {
+		lineeConsole [i + 1] = lineeConsole[i];
+	}
+	lineeConsole[0] = "("+to_string(contatoreStringhe)+") " + nuovaStringa;
+	contatoreStringhe += 1;
+}
+
 //disegno
 void disegna_entita(Entita& entita) {
 	switch(entita) {
@@ -119,12 +156,20 @@ void disegna_casella(Casella& casella) {
 	}
 }
 
+void stampa_linea_console(int i) {
+	if(i == 0)
+		cout << "=> ";
+	cout << lineeConsole[i];
+}
+
 int disegna_menu(elementoMenu menu[elementiMenuMappa], int& scelta) {
+	cout << "Comandi disponibili: " << endl;
 	for(int i = 0; i < elementiMenuMappa; i++) {
 		elementoMenu* elemento = &menu[i];
-		cout << (elemento -> ident) << " : " << (elemento -> tag) << endl;
+		cout << "\t" << (elemento -> ident) << " : " << (elemento -> tag) << endl;
 	}
 	char sceltaChar;
+	cout << "Digita un comando (o una sequenza di comandi): ";
 	cin >> sceltaChar;
 
 	for(int i = 0; i < elementiMenuMappa; i++) {
@@ -143,43 +188,51 @@ void esegui_scelta(int i, elementoMenu menu[elementiMenuMappa], Casella stanza[]
 void spostaGiocatore(int i, Casella stanza[], int& dimX) { //0 = su; 1 = sinistra; 2 = giù; 3 = destra
 	Casella* casellaGiocatore = &stanza[vettore2d_indice(posizioneGiocatore, dimX)];
 	casellaGiocatore -> entita = entitaVuota;
-	Casella* nuovaCasella;
+	vettore2d vecchiaPosizione = posizioneGiocatore;
+	string stringaDirezione;
 	switch(i) {
 		case 0:
 			posizioneGiocatore.y -= 1;
-			nuovaCasella = &stanza[vettore2d_indice(posizioneGiocatore, dimX)];
-			nuovaCasella -> entita = giocatore;
+			stringaDirezione = "verso l'alto.";
 			break;
 		case 1:
 			posizioneGiocatore.x -= 1;
-			nuovaCasella = &stanza[vettore2d_indice(posizioneGiocatore, dimX)];
-			nuovaCasella -> entita = giocatore;
+			stringaDirezione = "verso sinistra.";
 			break;
 		case 2:
 			posizioneGiocatore.y += 1;
-			nuovaCasella = &stanza[vettore2d_indice(posizioneGiocatore, dimX)];
-			nuovaCasella -> entita = giocatore;
+			stringaDirezione = "verso il basso.";
 			break;
 		case 3:
 			posizioneGiocatore.x += 1;
-			nuovaCasella = &stanza[vettore2d_indice(posizioneGiocatore, dimX)];
-			nuovaCasella -> entita = giocatore;
+			stringaDirezione = "verso destra.";
 			break;
+	}
+	Casella* nuovaCasella = &stanza[vettore2d_indice(posizioneGiocatore, dimX)];
+	if(nuovaCasella -> tipoCasella == casellaVuota) {
+		nuovaCasella -> entita = giocatore;
+		concatenaConsole("Ti sei spostato " + stringaDirezione);
+	} else {
+		casellaGiocatore -> entita = giocatore; //reimposta giocatore se ha colliso
+		posizioneGiocatore = vecchiaPosizione;
+		concatenaConsole("Hai incontrato un muro!");
 	}
 }
 
-void disegna_stanza(Casella stanza[], int& dimX, int& dimY) {
+void disegna_stanza(Casella stanza[], int& dimX, int& dimY) { //oltre alla stanza, si concatena sul lato destro la console
 	for(int y = 0; y < dimY; y++) {
 		for(int x = 0; x < dimX; x++) {
 			int indice = vettore2d_indice(x ,y, dimX);
 			disegna_casella(stanza[indice]);
 		}
+		cout << "| ";
+		stampa_linea_console(y);
 		cout << endl;
 	}
 }
 
 int main() {
-	srand(time(0)); //inizializza seed casuale con time
+	srand(25); //inizializza seed casuale con time
 	//inizializzazione livello
 	int dimX = dado(boundLivelloMinimi.x, boundLivelloMassimi.x);
 	int dimY = dado(boundLivelloMinimi.y, boundLivelloMassimi.y);
@@ -192,12 +245,15 @@ int main() {
 	//inizializzazione menu
 	elementoMenu menuMappa[elementiMenuMappa];
 	inizializza_menuMappa(menuMappa);
+	inizializza_console();
 
 	int scelta;
 	
 	while(true) {
+		clear();
 		//game loop
 		disegna_stanza(stanza, dimX, dimY);
+		separator(dimX);
 		int scelta = disegna_menu(menuMappa, scelta);
 		esegui_scelta(scelta, menuMappa, stanza, dimX);
 	}
