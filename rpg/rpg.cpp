6ab598c875b480f3a7afc9a3040d7ab82bc9bf72
Luca Seggiani <seggianiluca2004@gmail.com>
2023-10-18 15:30:05 +0200

    Risolto rendering menu e aggiunta funzione di spostamento giocatore

--- rpg/rpg.cpp
+++ rpg/rpg.cpp
@@ -10,12 +10,15 @@ struct vettore2d { int x; int y; };
 enum TipoCasella { muro, casellaVuota };
 enum Entita { giocatore, entitaVuota };
 struct Casella { TipoCasella tipoCasella; Entita entita; };
+vettore2d posizioneGiocatore; //la posizione del giocatore e' definita sia come entita che come coordinate... forse va cambiato
 vettore2d boundLivelloMinimi = {5,5};
 vettore2d boundLivelloMassimi = {55, 15};
 
 //elementi menu
-struct elementoMenu { char ident; char* tag; void *funzione(void); };
-const int elementiMenuMappa = 1; //fornisco una lunghezza fissa per il compilatore
+struct elementoMenu { char ident; char* tag; void (*funzione)(int, Casella*, int&); }; 	//le funzioni accettano un indice i equivalente alla chiamata effettuata dal giocatore, questo
+																						//è fatto per poter permettere comportamenti diversi alla solita funzione (e.g. spostaGiocatore)
+																						//inoltre accettano un riferimento alla stanza attuale e la dimensione orizzontale della suddetta
+const int elementiMenuMappa = 4; //fornisco una lunghezza fissa per il compilatore
 
 //utility livello
 int vettore2d_indice(vettore2d& vettore, int& dimX) { //converte un vettore bidimensionale in un indice array
@@ -68,12 +71,30 @@ void inizializza_giocatore(Casella stanza[], int dimX, int dimY) {
 	int posizione_giocatoreX = dado(1, dimX - 2);
 	int posizione_giocatoreY = dado(1, dimY - 2);
 	Casella* casellaGiocatore = &stanza[vettore2d_indice(posizione_giocatoreX, posizione_giocatoreY, dimX)];
+	posizioneGiocatore.x = posizione_giocatoreX;
+	posizioneGiocatore.y = posizione_giocatoreY;
 	casellaGiocatore -> entita = giocatore;
 }
 
+void spostaGiocatore(int i, Casella stanza[], int& dimX); //inizializzo per la generazione successiva del menù
+
 //generazione menu
 void inizializza_menuMappa(elementoMenu menuMappa[elementiMenuMappa]) {
-	
+	menuMappa[0].ident = 'w';
+	menuMappa[0].tag = "Vai su";
+	menuMappa[0].funzione = spostaGiocatore;
+
+	menuMappa[1].ident = 'a';
+	menuMappa[1].tag = "Vai a sinistra";
+	menuMappa[1].funzione = spostaGiocatore;
+
+	menuMappa[2].ident = 's';
+	menuMappa[2].tag = "Vai giu'";
+	menuMappa[2].funzione = spostaGiocatore;
+
+	menuMappa[3].ident = 'd';
+	menuMappa[3].tag = "Vai a destra";
+	menuMappa[3].funzione = spostaGiocatore;
 }
 
 //disegno
@@ -98,13 +119,53 @@ void disegna_casella(Casella& casella) {
 	}
 }
 
-int disegna_menu(elementoMenu menu[]) {
+int disegna_menu(elementoMenu menu[elementiMenuMappa], int& scelta) {
+	for(int i = 0; i < elementiMenuMappa; i++) {
+		elementoMenu* elemento = &menu[i];
+		cout << (elemento -> ident) << " : " << (elemento -> tag) << endl;
+	}
+	char sceltaChar;
+	cin >> sceltaChar;
+
+	for(int i = 0; i < elementiMenuMappa; i++) {
+		if(sceltaChar == menu[i].ident)
+			return i;
+	}
+
 	return 0;
 }
 
 //logica di gioco
-void spostaGiocatore() {
-	
+void esegui_scelta(int i, elementoMenu menu[elementiMenuMappa], Casella stanza[], int& dimX) {
+	(*menu[i].funzione) (i, stanza, dimX);
+}
+
+void spostaGiocatore(int i, Casella stanza[], int& dimX) { //0 = su; 1 = sinistra; 2 = giù; 3 = destra
+	Casella* casellaGiocatore = &stanza[vettore2d_indice(posizioneGiocatore, dimX)];
+	casellaGiocatore -> entita = entitaVuota;
+	Casella* nuovaCasella;
+	switch(i) {
+		case 0:
+			posizioneGiocatore.y -= 1;
+			nuovaCasella = &stanza[vettore2d_indice(posizioneGiocatore, dimX)];
+			nuovaCasella -> entita = giocatore;
+			break;
+		case 1:
+			posizioneGiocatore.x -= 1;
+			nuovaCasella = &stanza[vettore2d_indice(posizioneGiocatore, dimX)];
+			nuovaCasella -> entita = giocatore;
+			break;
+		case 2:
+			posizioneGiocatore.y += 1;
+			nuovaCasella = &stanza[vettore2d_indice(posizioneGiocatore, dimX)];
+			nuovaCasella -> entita = giocatore;
+			break;
+		case 3:
+			posizioneGiocatore.x += 1;
+			nuovaCasella = &stanza[vettore2d_indice(posizioneGiocatore, dimX)];
+			nuovaCasella -> entita = giocatore;
+			break;
+	}
 }
 
 void disegna_stanza(Casella stanza[], int& dimX, int& dimY) {
@@ -131,9 +192,14 @@ int main() {
 	//inizializzazione menu
 	elementoMenu menuMappa[elementiMenuMappa];
 	inizializza_menuMappa(menuMappa);
+
+	int scelta;
 	
-	//game loop
-	disegna_stanza(stanza, dimX, dimY);
-	int scelta = disegna_menu(menuMappa);
+	while(true) {
+		//game loop
+		disegna_stanza(stanza, dimX, dimY);
+		int scelta = disegna_menu(menuMappa, scelta);
+		esegui_scelta(scelta, menuMappa, stanza, dimX);
+	}
 	return 0;
 }
